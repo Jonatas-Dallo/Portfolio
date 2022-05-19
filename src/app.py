cd59568a41b50fde56cf330716cc333c1cec38fd
Jonatas <jonatasmathiasdallo@gmail.com>
2022-05-19 12:18:33 -0300

    projeto rodando localmente com flask

---
+++ src/app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+
+app = Flask("__name__")
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+@app.route('/int')
+def interesses():
+    return render_template('interesses.html')
+
+@app.route('/port')
+def portfolio():
+    return render_template('portfólio.html')
+
+
+if __name__ == "__ main__":
+    app.run(debug=True)
