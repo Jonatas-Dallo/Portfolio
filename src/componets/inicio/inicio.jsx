bf7dac634a384f6c4899ef821f508adf09e98712
Jonatas Mathias <jonatasmathiasdallo@gmail.com>
2024-10-06 01:56:53 -0300

    ajeitando component inicial

--- src/componets/inicio/inicio.jsx
+++ src/componets/inicio/inicio.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { useInView } from 'react-intersection-observer';
+import githubImagem from '../../assets/icons/github.png';
+import linkedinImagem from '../../assets/icons/linkedin.png';
 import './inicio.css';
 
 const Inicio = () => {
@@ -8,18 +10,26 @@ const Inicio = () => {
   return (
     <div ref={ref} className={`section ${inView ? 'visible' : 'hidden'}`}>
       <div className="inicio-card">
-        <div>
-            <p>Developer</p>
-            <img src="" alt="" />
-        </div>
-        <div>
+        <div className='inicio-texto'>
+          <div className='linha1'>
+            <div className='developer-container'>
+              <p className='developer-text'>
+                Developer
+                <a href="https://github.com/Jonatas-Dallo" target="_blank" rel="noopener noreferrer" className='circular-image-github'>
+                  <img src={githubImagem} alt="logo do github" />
+                </a>
+              </p>
+            </div>
             <p>Full Stack</p>
-            <img src="" alt="" />
-        </div>
-        <div>
-            <p>JonaTas Dallo</p>
-            <img src="" alt="" />
-            <img src="" alt="" />
+            <div className='nome-container'>
+              <p className='nome-text'>
+                Jonatas Dallo
+                <a href="https://www.linkedin.com/in/jonatas-mathias-147638206/" target="_blank" rel="noopener noreferrer" className='circular-image-linkedin'>
+                  <img src={linkedinImagem} alt="logo do linkedin" />
+                </a>
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
